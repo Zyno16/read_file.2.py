e97f8fa3b4fd9169453f4c2cc8181e20fdfe3d80
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-08 20:06:54 +0000

    Add files via upload

---
+++ read_file.2.py
@@ -0,0 +1,10 @@
+ 
+ #this other way to read the file and calculate
+ #  how much letter ie got 
+fhand = open("parcing_extracting.py")
+x =fhand.read()
+print(x)
+y = len(x)
+print (y)
+print (x[0 : 10])
+
